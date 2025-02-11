194ac26d0c7c1de22c819bf2a295293591a46b3c
JustCheburek <artemiznakhdki@gmail.com>
2025-02-11 20:09:30 +1000

    bkp

--- app/bkp/page.tsx
+++ app/bkp/page.tsx
@@ -1,13 +1,18 @@
-import {BlockQuote, H2, H3, P, Ul} from "@components/text";
+import {BlockQuote, H3, P, Ul} from "@components/text";
 import {ComponentPropsWithoutRef, PropsWithChildren} from "react";
 import {cn} from "@server/cn";
 import Image from "next/image";
 
-function Box({children, className = "", ...props}: PropsWithChildren<ComponentPropsWithoutRef<"div">>) {
+type Box = {
+	img?: boolean
+} & ComponentPropsWithoutRef<"div">
+
+function Box({children, className = "", img = false, ...props}: PropsWithChildren<Box>) {
 	return (
 			<div
 					className={cn(
-							"relative flex flex-col justify-center items-center gap-2 border border-light-gray rounded-lg p-4 bg-gray",
+							"relative flex flex-col justify-center items-center gap-4 border border-light-gray rounded-lg p-4 bg-gray",
+							{"overflow-clip min-h-64": img},
 							className
 					)}
 					{...props}
@@ -17,11 +22,21 @@ function Box({children, className = "", ...props}: PropsWithChildren<ComponentPr
 	)
 }
 
+type Heading = ComponentPropsWithoutRef<"h3">
+
+function Heading({children, className = "", ...props}: PropsWithChildren<Heading>) {
+	return (
+			<H3 className={cn("text-center", className)} {...props}>
+				{children}
+			</H3>
+	)
+}
+
 export default function BKP() {
 	return (
 			<main className="container mx-auto">
-				<div className="grid grid-cols-3 gap-12">
-					<div className="relative grid grid-cols-7 gap-2 *:border-light-gray">
+				<div className="grid md:grid-cols-2 lg:grid-cols-3 gap-12 pt-6 pb-12">
+					<div className="relative grid grid-cols-7 gap-2 *:border-light-gray min-h-40">
 						<P className="absolute left-1.5 top-[50%] translate-y-[-50%] [writing-mode:vertical-lr] rotate-180">
 							Вертикальный
 						</P>
@@ -42,9 +57,9 @@ export default function BKP() {
 					</div>
 
 					<Box>
-						<H3>
+						<Heading>
 							Секции и блоки
-						</H3>
+						</Heading>
 						<Ul>
 							<li>
 								<P>
@@ -60,9 +75,9 @@ export default function BKP() {
 					</Box>
 
 					<Box>
-						<H3>
+						<Heading>
 							Мостовые краны
-						</H3>
+						</Heading>
 						<div>
 							<Ul>
 								<li>
@@ -77,15 +92,37 @@ export default function BKP() {
 						</div>
 					</Box>
 
-					<Box className="grid p-8 py-16">
-						<div>
-							<H3>
-								Производственная мощность
-							</H3>
-							<P>
-								по переработке стального проката
-							</P>
-						</div>
+					<Box>
+						<Heading>
+							Установлены
+						</Heading>
+						<Ul>
+							<li>
+								<P>
+									Машины резки и правки профилей
+								</P>
+							</li>
+							<li>
+								<P>
+									Линии изготовления тавровых блоков, плоских секций, криволинейных и микропанеле
+								</P>
+							</li>
+							<li>
+								<P>
+									Станки для снятия заусенцев и скругления кромок
+								</P>
+							</li>
+						</Ul>
+					</Box>
+
+					<Box img>
+						<Image src="/bkp/building.jpg" alt="Сварка" fill className="hover:scale-110 duration-500"/>
+					</Box>
+
+					<Box>
+						<Heading>
+							Производственная мощность
+						</Heading>
 						<Ul>
 							<li>
 								<P>
@@ -100,42 +137,42 @@ export default function BKP() {
 						</Ul>
 					</Box>
 
-					<Box className="overflow-clip col-span-2">
+					<Box>
+						<Heading>
+							Высокоточная роботизированная система
+						</Heading>
+						<P>
+							Резка металла с точностью позиционирования до 0,5 мм.
+						</P>
+					</Box>
+
+					<Box img className="lg:col-span-2">
 						<Image src="/bkp/welding.png" alt="Сварка" fill className="hover:scale-110 duration-500"/>
 					</Box>
-				</div>
 
-				<br/>
-				<P>
-					Высокоточная роботизированная система, позволяющая совершать
-					операции резки металла с точностью позиционирования до 0,5 мм.
-				</P>
-				<br/>
-				<P>
-					Установлены:
-				</P>
-				<ul className="ml-8 list-disc marker:text-neutral-300/70">
-					<li>
-						<P>
-							Машины резки и правки профилей
-						</P>
-					</li>
-					<li>
-						<P>
-							Линии изготовления тавровых блоков, плоских секций, криволинейных и микропанеле
-						</P>
-					</li>
-					<li>
-						<P>
-							Станки для снятия заусенцев и скругления кромок
-						</P>
-					</li>
-				</ul>
-				<br/>
-				<P>
-					Специальная сварочная лаборатория, которая предназначена для подготовки высококвалифицированных
-					специалистов, для проведения испытаний инновационного сварочного оборудования и технологий.
-				</P>
+					<Box img className="lg:col-span-2">
+						<Image src="/people/group.jpg" alt="Сварка" fill className="hover:scale-110 duration-500"/>
+					</Box>
+
+					<Box>
+						<Heading>
+							Специальная сварочная лаборатория
+						</Heading>
+						<Ul>
+							<li>
+								<P>
+									Подготовка высококвалифицированных
+									специалистов
+								</P>
+							</li>
+							<li>
+								<P>
+									Проведения испытаний инновационного сварочного оборудования и технологий
+								</P>
+							</li>
+						</Ul>
+					</Box>
+				</div>
 			</main>
 	)
 }
