493dc42f24ddd513d07468a8a59729fb8e90d338
JustCheburek <artemiznakhdki@gmail.com>
2025-02-12 12:23:03 +1000

    images + bkp

--- app/bkp/page.tsx
+++ app/bkp/page.tsx
@@ -20,29 +20,13 @@ export default function BKP() {
 				</Name>
 
 				<Container>
-					<div className="relative grid grid-cols-7 gap-2 *:border-light-gray min-h-40">
-						<P className="absolute left-4 top-[50%] translate-y-[-50%] [writing-mode:vertical-lr] rotate-180">
-							Вертикальный
-						</P>
-						<div className="rounded-tl-lg rounded-bl-lg border-l border-t border-b bg-neutral-100 dark:bg-gray"/>
-						<div className="relative grid col-span-6 gap-2 *:border-light-gray">
-							<H3 className="absolute top-8 left-[50%] translate-x-[-50%]">
-								Пролёты
-							</H3>
-							<P className="w-max absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]">
-								5 горизонтальных
-							</P>
-							<div className="rounded-tr-lg border-t bg-neutral-100 dark:bg-gray border-r"/>
-							<div className="bg-neutral-100 dark:bg-gray border-r"/>
-							<div className="bg-neutral-100 dark:bg-gray border-r"/>
-							<div className="bg-neutral-100 dark:bg-gray border-r"/>
-							<div className="rounded-br-lg border-b bg-neutral-100 dark:bg-gray border-r"/>
-						</div>
-					</div>
+					<Box img>
+						<Img src="/block.jpg" alt="Секция судна"/>
+					</Box>
 
 					<Box>
 						<Heading>
-							Секции и блоки
+							Изготовление секций и блоков судна
 						</Heading>
 						<Ul>
 							<li>
@@ -56,51 +40,6 @@ export default function BKP() {
 								</P>
 							</li>
 						</Ul>
-					</Box>
-
-					<Box>
-						<Heading>
-							Мостовые краны
-						</Heading>
-						<div>
-							<Ul>
-								<li>
-									<P>
-										до 120 тонн
-									</P>
-								</li>
-							</Ul>
-							<BlockQuote>
-								«Работают один за другим»
-							</BlockQuote>
-						</div>
-					</Box>
-
-					<Box>
-						<Heading>
-							Установлены
-						</Heading>
-						<Ul>
-							<li>
-								<P>
-									Машины резки и правки профилей
-								</P>
-							</li>
-							<li>
-								<P>
-									Линии изготовления тавровых блоков, плоских секций, криволинейных и микропанеле
-								</P>
-							</li>
-							<li>
-								<P>
-									Станки для снятия заусенцев и скругления кромок
-								</P>
-							</li>
-						</Ul>
-					</Box>
-
-					<Box img>
-						<Img src="/bkp/building.jpg" alt="Цех"/>
 					</Box>
 
 					<Box>
@@ -121,6 +60,80 @@ export default function BKP() {
 						</Ul>
 					</Box>
 
+					<div className="relative grid grid-cols-7 gap-2 *:border-light-gray min-h-40">
+						<P className="absolute left-[3%] top-[50%] translate-y-[-50%] [writing-mode:vertical-lr] rotate-180">
+							Вертикальный
+						</P>
+						<div className="rounded-tl-lg rounded-bl-lg border-l border-t border-b bg-neutral-100 dark:bg-gray"/>
+						<div className="relative grid col-span-6 gap-2 *:border-light-gray">
+							<H3 className="absolute top-[30%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+								Пролёты
+							</H3>
+							<P className="w-max absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]">
+								5 горизонтальных
+							</P>
+							<div className="rounded-tr-lg border-t bg-neutral-100 dark:bg-gray border-r"/>
+							<div className="bg-neutral-100 dark:bg-gray border-r"/>
+							<div className="bg-neutral-100 dark:bg-gray border-r"/>
+							<div className="bg-neutral-100 dark:bg-gray border-r"/>
+							<div className="rounded-br-lg border-b bg-neutral-100 dark:bg-gray border-r"/>
+						</div>
+					</div>
+
+					<Box img>
+						<Img src="/bkp/building.jpg" alt="Цех"/>
+					</Box>
+
+					<Box>
+						<Heading>
+							Мостовые краны
+						</Heading>
+						<div>
+							<Ul>
+								<li>
+									<P>
+										до 120 тонн
+									</P>
+								</li>
+							</Ul>
+							<BlockQuote>
+								«Работают один за другим»
+							</BlockQuote>
+						</div>
+					</Box>
+
+					<Box img className="lg:col-span-2">
+						<Img src="/bkp/people_welding.jpg" alt="Сварка"/>
+					</Box>
+
+					<Box>
+						<Heading>
+							Установлены
+						</Heading>
+						<Ul>
+							<li>
+								<P>
+									Линии термической резки металла
+								</P>
+							</li>
+							<li>
+								<P>
+									Машины резки и правки профилей
+								</P>
+							</li>
+							<li>
+								<P>
+									Линии изготовления тавровых блоков, плоских секций, криволинейных и микропанеле
+								</P>
+							</li>
+							<li>
+								<P>
+									Станки для снятия заусенцев и скругления кромок
+								</P>
+							</li>
+						</Ul>
+					</Box>
+
 					<Box className="text-center">
 						<Heading>
 							Высокоточная роботизированная система
@@ -131,7 +144,7 @@ export default function BKP() {
 					</Box>
 
 					<Box img className="lg:col-span-2">
-						<Img src="/bkp/welding.jpg" alt="Сварка"/>
+						<Img src="/bkp/auto_welding.jpg" alt="Сварка"/>
 					</Box>
 
 					<Box img className="lg:col-span-2">
